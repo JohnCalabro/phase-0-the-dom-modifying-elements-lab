45c5d5b21bcc75b5c03d6abcfd64e0b05662f2ab
johncalabro <j.calabro4@gmail.com>
2022-03-18 12:58:43 -0400

    commit tree 7f19e0e7d10fbe0447d0aca50c53ed960daa79aa

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const element = document.getElementById("main");
+element.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id="victory"
+newHeader.textContent = "John is the champion" 
+//<h1></h1>
+
+//<h1 id= "victory">John is the champion</h1>
+//<main id="main"></main>
+
